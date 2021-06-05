a386eeeef0ba40e362688a97d678e6087535c800
EtshD1 <mohamed.h.abouelenin@gmail.com>
2021-06-05 12:25:01 +0200

    Add Authentication Elements and their JS selectors

--- public/app.js
+++ public/app.js
@@ -1,5 +1,17 @@
 (() => {
   const auth = firebase.auth();
 
+  const elements = {
+    buttons: {
+      signIn: document.querySelector("#signIn"),
+      signOut: document.querySelector("#signOut"),
+    },
+    sections: {
+      signedIn: document.querySelector("#signedIn"),
+      signedOut: document.querySelector("#signedOut")
+    },
+    username: document.querySelector("#user-name")
+  }
+
   console.log(auth);
 })();
