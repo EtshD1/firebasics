bc80049a459a1752231cbeee38abd55375c5dff3
EtshD1 <mohamed.h.abouelenin@gmail.com>
2021-06-05 15:19:10 +0200

    log in logic

--- public/app.js
+++ public/app.js
@@ -1,6 +1,8 @@
 (() => {
   const auth = firebase.auth();
 
+  const provider = new firebase.auth.GoogleAuthProvider();
+
   const elements = {
     buttons: {
       signIn: document.querySelector("#signIn"),
@@ -12,6 +14,24 @@
     },
     username: document.querySelector("#user-name")
   }
+
+  elements.buttons.signIn.addEventListener("click", () => {
+    auth.signInWithPopup(provider);
+  });
+
+  elements.buttons.signOut.addEventListener("click", () => auth.signOut());
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      elements.sections.signedIn.removeAttribute("hidden");
+      elements.sections.signedOut.setAttribute("hidden", true);
+      elements.username.textContent = user.displayName;
+    } else {
+      elements.sections.signedOut.removeAttribute("hidden");
+      elements.sections.signedIn.setAttribute("hidden", true);
+      elements.username.textContent = "";
+    }
+  })
 
   console.log(auth);
 })();
