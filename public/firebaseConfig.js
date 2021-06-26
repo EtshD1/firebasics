e9269b14bf7c4d7cade3fcc3641c50593947f992
EtshD1 <mohamed.h.abouelenin@gmail.com>
2021-06-26 12:08:21 +0200

    removed the need to use firebase emulator

---
+++ public/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAfn2m2n-R8DTmysjUIK_FnCY1mCJUbtUQ",
+  authDomain: "firebasics-1720b.firebaseapp.com",
+  projectId: "firebasics-1720b",
+  storageBucket: "firebasics-1720b.appspot.com",
+  messagingSenderId: "332787525406",
+  appId: "1:332787525406:web:d17caf55016f16ac5b8b0f",
+  measurementId: "G-YR8KPX9GCC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
